637ca622a4c557d279e8f28473f188142037c3ce
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-10-02 01:02:05 +1100

    Add Book object

--- library.js
+++ library.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// (yes, the original task was with a constructor)
+// (but I like factories better)
+const Book = function(title, author, pages, read) {
+    const info = function() {
+        return (`${this.title}, by ${this.author}, ${this.pages} pages, read: ${this.read}`)
+    }
+
+    return {
+        title,
+        author,
+        pages,
+        read,
+        info,
+    }
+}
+
+const shelf = []
+
+shelf.push(Book(
+    "The Hobbit",
+    "Jolkein Rolkein Rolkein Tolkein",
+    "5",
+    true));
