d2f98e74356a7205873ed75d0bb299ec6ab8fc15
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-10-02 22:39:52 +1100

    Add JS table-processing plan comments

--- library.js
+++ library.js
@@ -49,6 +49,28 @@ addBook(Book(
 	true
 ));
 
+// What I'd really, really want is the ability
+// to look for the appropriate header for any
+// given table-cell that is nominated.
+//	You'd think this would be a one-step job, but it
+//	actually doesn't seem to exist in such a format.
+// I see a few methods.
+//	From a cell:
+//		Get the parent (row),
+//		determine own position in row,
+//		Get parent's parent (table) -> get headers,
+//		walk along headers same distance, get header.
+// 	Create an array that positively links *position* to *object member*.
+//		Right now that's hard-coded.
+//		For creating a row, you iterate over that array
+//		and take the appropriate object member for each row.
+//	For convenience I should definitely put the member into the td's class.
+//	and each row (tr) can have the object ID for class too.
+//		The ID column is just for user information.
+
+// you'll need node.parentElement
+
+
 const addRow = function() {
 	// recordTableBody
 	const tr = document.createElement("tr");
