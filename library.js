34635dc5e3174c710921accf13d4a9efa3bfb593
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-10-02 11:59:29 +1100

    Add book-adding functions. Also indentation.

--- library.js
+++ library.js
@@ -3,23 +3,37 @@
 // (yes, the original task was with a constructor)
 // (but I like factories better)
 const Book = function(title, author, pages, read) {
-    const info = function() {
-        return (`${this.title}, by ${this.author}, ${this.pages} pages, read: ${this.read}`)
-    }
+	const info = function() {
+		return (`${this.title}, by ${this.author}, ${this.pages} pages, read: ${this.read}`)
+	}
 
-    return {
-        title,
-        author,
-        pages,
-        read,
-        info,
-    }
+	return {
+		title,
+		author,
+		pages,
+		read,
+		info,
+	}
 }
 
-const shelf = []
+// this could be an object
+const books = []
 
-shelf.push(Book(
-    "The Hobbit",
-    "Jolkein Rolkein Rolkein Tolkein",
-    "5",
-    true));
+// it seems silly for this to be its own function, but whatever
+const addBook = function(_book) {
+	books.push(_book)
+}
+
+addBook(Book(
+		"The Hobbit",
+	"Jolkein Rolkein Rolkein Tolkein",
+	"5",
+	true
+));
+
+
+books.push(Book(
+	"The Hobbit",
+	"Jolkein Rolkein Rolkein Tolkein",
+	"5",
+	true));
